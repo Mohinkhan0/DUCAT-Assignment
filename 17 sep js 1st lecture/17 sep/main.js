bdbfcf32099d13feaf64c917877b66e51bfe8bf1
Mohin khhan <117769740+Mohinkhan0@users.noreply.github.com>
2024-10-18 09:49:29 +0530

    Add files via upload

---
+++ 17 sep js 1st lecture/17 sep/main.js
@@ -0,0 +1,15 @@
+// document.write("external js");
+console.log("this is external\n break line");
+
+// identifier
+
+var a=25;
+// document.write("result="+a);
+
+
+// string
+
+
+var data="mohin khan";
+console.log(typeof(data));
+// document.write("your name is ="+data)
---
+++ 17 sep js 1st lecture/18 sep operator/oper.js
@@ -0,0 +1,21 @@
+var a=50;
+var b=20;
+// console.log(a+b);
+// console.log(a-b);
+
+// console.log(a*b);
+
+// console.log(a/b);
+// console.log(a**b);
+
+
+// assignment oper
+
+
+// console.log(a+=50);
+// console.log(a-=50);
+// var c=a*50;
+// console.log(c);
+// console.log(a*=50);
+
+
