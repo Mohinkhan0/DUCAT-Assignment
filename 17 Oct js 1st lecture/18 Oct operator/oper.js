7aa071391c21f0d6638a481d3f6509a363b83197
Mohin khhan <117769740+Mohinkhan0@users.noreply.github.com>
2024-10-18 10:27:05 +0530

    Add files via upload

---
+++ 17 Oct js 1st lecture/18 Oct operator/oper.js
@@ -0,0 +1,38 @@
+var a=50;
+var b=20;
+// console.log(a+b);
+// console.log(a-b);
+
+// console.log(a*b);
+
+// console.log(a/b);
+// console.log(a**b);
+
+
+//                                                     assignment oper
+
+
+// console.log(a+=50);
+// console.log(a-=50);
+// var c=a*50;
+// console.log(c);
+// console.log(a*=50);
+
+//                                          pre and post icriment 
+// console.log(a++);
+// console.log(a);
+
+// console.log(++a);
+// console.log(a);
+
+
+// console.log(a--);
+// console.log(a);
+
+// console.log(--a);
+// console.log(a);
+
+// logical operator
+
+console.log(10<20 && 20==20);
+
