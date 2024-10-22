e663fad1423c75733d3193714f2a365284913874
Mohin khhan <117769740+Mohinkhan0@users.noreply.github.com>
2024-10-22 10:55:28 +0530

    Add files via upload

---
+++ 17 Oct js 1st lecture/22 oct conditional statements/conditional.js
@@ -0,0 +1,14 @@
+var a=20;
+var mail="mohin@gmail.com";
+var pass="12345"
+
+
+// if (a>10)
+// {
+//   console.log(a);
+// }
+
+if(mail==="mohin@gmail.com" && pass==="12345")
+{
+  console.log("user Loged In ");
+}
